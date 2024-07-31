b172381545946c6a448129d599d2197de48a37f2
wild_duck <wezonaser50@gmail.com>
2024-07-31 15:12:08 +0300

    feat: init

---
+++ mix.exs
@@ -0,0 +1,31 @@
+defmodule LibGen.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :lib_gen,
+      version: "0.1.0",
+      elixir: "~> 1.17-rc",
+      escript: [main_module: DuckGen],
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:optimus, "~> 0.2"}
+
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
